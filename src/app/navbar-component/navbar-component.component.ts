7566dfb6821a6ec090770d22be0693651990140c
Burke Livingston <burkel24@gmail.com>
2017-09-30 14:52:39 -0400

    Add battery to navbar

--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+const BATTERY_DECAY_INTERVAL_MS = 2500;
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar-component.component.html',
@@ -8,6 +10,8 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class NavbarComponentComponent implements OnInit {
   public title = '';
+  public batteryPercent = 100;
+
   private urlMap = {
     '/': 'Terminal'
   };
@@ -20,6 +24,28 @@ export class NavbarComponentComponent implements OnInit {
     this.Router.events
       .filter(evt => evt instanceof NavigationEnd)
       .subscribe(() => this.title = this.urlMap[this.Router.url]);
+
+    const intervalId = setInterval(() => {
+      if (this.batteryPercent <= 1) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      this.batteryPercent--;
+    }, BATTERY_DECAY_INTERVAL_MS);
   }
 
+  get batteryIconClass() {
+    if (this.batteryPercent < 10) {
+      return 'fa-battery-0';
+    } else if (this.batteryPercent < 35) {
+      return 'fa-battery-1';
+    } else if (this.batteryPercent < 55) {
+      return 'fa-battery-2';
+    } else if (this.batteryPercent < 85) {
+      return 'fa-battery-3';
+    }
+
+    return 'fa-battery';
+  }
 }
