eb4c68a4def63ce13bb209dd5ad803aaddcfa25b
Burke Livingston <burkel24@gmail.com>
2017-10-07 16:55:12 -0400

    Working resizing

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, OnChanges, OnInit, Input, ViewChild  } from '@angular/core';
+import { Component, ComponentFactoryResolver, HostListener, OnChanges, OnInit, Input, ViewChild  } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 
 import { ApplicationBase } from '../applicationBase';
@@ -6,6 +6,7 @@ import { WindowContentHostDirective } from '../window/windowContentHost.directiv
 import { WindowManagerService, WindowState } from '../window-manager.service';
 
 const RETRY_INTERVAL_MS = 200;
+const MINIMUM_DIMENSION_PX = 75;
 
 @Component({
   selector: 'app-window',
@@ -15,11 +16,15 @@ const RETRY_INTERVAL_MS = 200;
 export class WindowComponent implements OnChanges, OnInit {
   private stateSubscription: Subscription;
   private state: WindowState;
+  private tempState: WindowState;
+  private lastMouseCoords: { x: number, y: number };
   private componentRef;
-  private window;
-  public title: string;
+  private lockX = false;
+  private lockY = false;
+
   @Input() app: ApplicationBase;
   @ViewChild(WindowContentHostDirective) contentHost: WindowContentHostDirective;
+  @ViewChild('appWindow') window;
 
   constructor(
     private resolver: ComponentFactoryResolver,
@@ -48,5 +53,71 @@ export class WindowComponent implements OnChanges, OnInit {
     this.stateSubscription = this.windowManager.registerWindow(state => {
       this.state = state;
     });
+  }
+
+  get top() {
+    if (!this.state) { return; }
+    return `${this.tempState ? this.tempState.top : this.state.top }px`;
+  }
+
+  get bottom() {
+    if (!this.state) { return; }
+    return `${this.tempState ? this.tempState.bottom : this.state.bottom }px`;
+  }
+
+  get left() {
+    if (!this.state) { return; }
+    return `${this.tempState ? this.tempState.left : this.state.left }px`;
+  }
+
+  get right() {
+    if (!this.state) { return; }
+    return `${this.tempState ? this.tempState.right : this.state.right }px`;
+  }
+
+  onResizeStart(evt, lockX=false, lockY=false) {
+    this.tempState = Object.assign({}, this.state);
+    this.lockX = lockX;
+    this.lockY = lockY;
+
+    this.lastMouseCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(evt) {
+    if (!this.tempState || !this.lastMouseCoords) { return; }
+
+    const mouseCoords = { x: evt.clientX, y: evt.clientY },
+      dx = this.lockX ? 0 : mouseCoords.x - this.lastMouseCoords.x,
+      dy = this.lockY ? 0 : mouseCoords.y - this.lastMouseCoords.y;
+
+    if (!dx && !dy) { return; }
+
+    const elm = this.window.nativeElement;
+
+    if (dx && (elm.clientWidth + dx) >= MINIMUM_DIMENSION_PX) {
+      this.tempState.right -= dx;
+      elm.style.right = `${this.tempState.right}px`;
+    }
+
+    if (dy && (elm.clientHeight + dy) >= MINIMUM_DIMENSION_PX) {
+      this.tempState.bottom -= dy;
+      elm.style.bottom = `${this.tempState.bottom}px`;
+    }
+
+    this.lastMouseCoords = mouseCoords;
+  }
+
+  @HostListener('document:mouseup', [])
+  onMouseUp() {
+    if (!this.tempState || !this.lastMouseCoords) { return; }
+
+    this.state = Object.assign({}, this.state, this.tempState);
+    this.tempState = null;
+    // TODO update service
+    this.lastMouseCoords = null;
   }
 }
